d1ddc45af4fd3aef58f256db5cf651a32fa6d277
tippa024 <sai.srinivas024@gmail.com>
2023-03-23 01:13:46 +0530

    Fix

--- Components/SmallCard.js
+++ Components/SmallCard.js
@@ -3,7 +3,7 @@ import Image from "next/image"
 function SmallCard({ img, distance, location }) 
     {
     return (
-        <div className="flex items-center m-1 p-1 space-x-4 cursor-pointer hover:bg-gray-200 rounded-xl hover:scale-105 transition duration-200 ease-out">
+        <div className="relative flex items-center m-1 p-1 space-x-4 cursor-pointer hover:bg-gray-200 rounded-xl hover:scale-105 transition duration-200 ease-out">
             {/* Left */}
             <div className="relative h-16 w-16">
                 <Image
